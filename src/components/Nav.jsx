a47e85c67f417b58f75f17261d9dc8bb5170a5d7
komalsingh02 <komalsingh23748@gmail.com>
2024-12-20 08:02:48 +0530

    Starting Setup

---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div className="text-white">
+      <h1>React.</h1>
+      <ul>
+        <li>Home</li>
+        <li>Company</li>
+        <li>Resource</li>
+        <li>About</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;
