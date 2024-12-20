fc09abcd74451cd3b61b34d1aafb48a417c3b808
komalsingh02 <komalsingh23748@gmail.com>
2024-12-20 08:09:51 +0530

    First Styling of Nav Bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <div className="text-white">
-      <h1>React.</h1>
-      <ul>
-        <li>Home</li>
-        <li>Company</li>
-        <li>Resource</li>
-        <li>About</li>
+    <div className=" flex justify-between  items-center h-24  max-w-[1440px] px-4 mx-auto text-white">
+      <h1 className="font-bold text-3xl text-[#00fd90]">React.</h1>
+      <ul className="flex">
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Resource</li>
+        <li className="p-4">About</li>
       </ul>
     </div>
   );
